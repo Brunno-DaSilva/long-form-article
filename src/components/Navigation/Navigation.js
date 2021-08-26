27e48cd97957322f900790c888b89a9b437c686f
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-08-26 18:10:01 -0500

    created file structure and components folder, added NAV folder

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navigation.css";
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <p>NAVIGATION</p>
+    </div>
+  );
+};
+
+export default Navigation;
