3ecf08ef2361382a0f42c9c8406db08e718e1903
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-08-27 17:02:05 -0500

    created the ABOUT Nav_DATA

--- src/NavigationData.js
+++ src/NavigationData.js
@@ -28,20 +28,92 @@ const NAV_DATA = [
     selected: false,
     items: [
       {
-        name: "District News",
-        url: "https://www.friscoisd.org/news?n=news",
+        name: "Board of Trustees",
+        url: "https://www.friscoisd.org/about/board-of-trustees/",
       },
       {
-        name: "Inside Frisco ISD",
-        url: "https://www.friscoisd.org/news?n=inside",
+        name: "Bond Programs",
+        url: "https://www.friscoisd.org/about/bond-programs",
       },
       {
-        name: "Connect With Frisco ISD",
-        url: "https://www.friscoisd.org/departments/communications/connect-with-us",
+        name: "Calendars",
+        url: "https://www.friscoisd.org/about/resources-and-information/school-calendars",
       },
       {
-        name: "Frisco ISD Athletics News",
-        url: "https://www.friscoisdsports.com/news",
+        name: "Community Folder",
+        url: "https://www.friscoisd.org/departments/communications/community-folder/elementary-community-folder",
+      },
+      {
+        name: "District Leadership",
+        url: "https://www.friscoisd.org/about/district-leadership/leaders",
+      },
+      {
+        name: "District of Innovation",
+        url: "https://www.friscoisd.org/departments/district-of-innovation/home",
+      },
+      {
+        name: "District Overview",
+        url: "https://www.friscoisd.org/about/district-overview/home",
+      },
+      {
+        name: "Mission, Vision & Beliefs",
+        url: "https://www.friscoisd.org/about/district-overview/mission-vision-beliefs",
+      },
+      {
+        name: "Early History",
+        url: "https://www.friscoisd.org/about/early-history/early-history",
+      },
+      {
+        name: "Enrollment",
+        url: "https://www.friscoisd.org/about/enrollment",
+      },
+      {
+        name: "Facility Map",
+        url: "https://www.friscoisd.org/about/district-facility-map",
+      },
+      {
+        name: "Financial Transparency",
+        url: "https://www.friscoisd.org/departments/finance/financial-transparency",
+      },
+      {
+        name: "Government Affairs",
+        url: "https://www.friscoisd.org/about/government-affairs",
+      },
+      {
+        name: "New to Frisco ISD",
+        url: "https://www.friscoisd.org/about/new-to-frisco-isd",
+      },
+      {
+        name: "Public Information Requests",
+        url: "https://www.friscoisd.org/about/resources-and-information/open-records-requests",
+      },
+      {
+        name: "Staff Email Search",
+        url: "https://www.friscoisd.org/about/resources-and-information/staff-email-search",
+      },
+      {
+        name: "Strategic Planning",
+        url: "https://www.friscoisd.org/about/strategic-planning/home",
+      },
+      {
+        name: "Parents",
+        url: "https://www.friscoisd.org/parents",
+        class: "btn__about",
+      },
+      {
+        name: "Students",
+        url: "https://www.friscoisd.org/students",
+        class: "btn__about",
+      },
+      {
+        name: "Community",
+        url: "https://www.friscoisd.org/community/community",
+        class: "btn__about",
+      },
+      {
+        name: "Staff",
+        url: "https://portal.friscoisd.org/LoginPolicy.jsp",
+        class: "btn__about",
       },
     ],
   },
