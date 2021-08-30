8b9855adb5e453ed0bdb61aaac071577738adef0
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-08-30 14:52:03 -0500

    Created simple data for article

---
+++ src/ArticleData.js
@@ -0,0 +1,53 @@
+const ARTICLE_DATA = [
+  {
+    id: 1,
+    landingPageURL_img:
+      "https://res.cloudinary.com/duprwuo4j/image/upload/v1604069785/erika-fletcher-MZxqc6n9qCw-unsplash_1_ajkdux.jpg",
+    landingPageURL_video:
+      "https://res.cloudinary.com/duprwuo4j/video/upload/v1630352010/school_kvpusu.mp4",
+    parallaxUrl_img:
+      "https://res.cloudinary.com/duprwuo4j/image/upload/v1604074385/babyboy_pjrncc.jpg",
+    header: "The Woman who fell f5rom the Sky",
+    subHeader: "A new Disease",
+    articleSummary:
+      "Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.",
+    imageGallery: [
+      {
+        name: "Class Room",
+        description: "students in the class room",
+        imageURL:
+          "https://res.cloudinary.com/duprwuo4j/image/upload/v1598505291/neonbrand-zFSo6bnZJTw-unsplash_1_q3dvgg.jpg",
+        imageCredits: "online source by Bruno DaSilva",
+      },
+      {
+        name: "Construction",
+        description: "some construction taking place",
+        imageURL:
+          "https://res.cloudinary.com/duprwuo4j/image/upload/v1602433230/hc-const_vxrkcn.jpg",
+        imageCredits: "Online source by Bruno DaSilva",
+      },
+      {
+        name: "Virtual Student",
+        description: "Student with headphones",
+        imageURL:
+          "https://res.cloudinary.com/duprwuo4j/image/upload/v1598505021/ben-mullins-je240KkJIuA-unsplash_1_svdhzv.jpg",
+        imageCredits: "Online source by Bruno DaSilva",
+      },
+      {
+        name: "Back to School",
+        description: "Back to School sign",
+        imageURL:
+          "https://res.cloudinary.com/duprwuo4j/image/upload/v1598505164/deleece-cook-zzjLGF_6dx4-unsplash_1_hjkqeh.jpg",
+        imageCredits: "Online source by Bruno DaSilva",
+      },
+    ],
+    blockquote:
+      "Be strong and courageous. Do not be afraid; do not be discouraged, for the LORD your God will be with you wherever you go",
+    blockquoteAuthor: "Joshua 1:9",
+    authorName: "Bruno DaSilva",
+    readingTime: 5,
+    DateTime: "March 26, 1989",
+  },
+];
+
+export default ARTICLE_DATA;
