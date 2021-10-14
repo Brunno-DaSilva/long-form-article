e4f98021c1fe28f64476c10f24d860fede67fbd1
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-10-14 18:55:37 -0500

    Added the search component

---
+++ src/components/Search/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./search.css";
+
+const Search = () => {
+  return (
+    <div className="Search">
+      <div>Search</div>
+    </div>
+  );
+};
+
+export default Search;
