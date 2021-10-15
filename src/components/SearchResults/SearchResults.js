01b9e40abdb48fba92de60bbb56c909669e63196
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-10-14 19:15:27 -0500

    changed the search component name to search results

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./searchResults.css";
+
+const SearchResults = () => {
+  return (
+    <div className="SearchResults">
+      <div>Search</div>
+    </div>
+  );
+};
+
+export default SearchResults;
