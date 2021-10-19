cddc282cee34e2931bf3d317d013385aa1090f49
brunno-dasilva <hi.dasilvab@gmail.com>
2021-10-19 09:39:01 -0500

    commented out search COMP code, add reactspring imagegallery comp

---
+++ src/components/ImageGalleryA/ImageGalleryWithSprings.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ImageGalleryWithSprings = () => {
+  return (
+    <div className="ImageGalleryWithSprings">
+      <div></div>
+    </div>
+  );
+};
+
+export default ImageGalleryWithSprings;
